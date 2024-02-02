6fda33c38a948f7c6ab93279355c921c993c8083
aryan2397 <135629240+aryan2397@users.noreply.github.com>
2024-02-03 03:06:29 +0530

    Initial commit

---
+++ routes/task.js
@@ -0,0 +1,13 @@
+import express from "express";
+import { deleteTask, getMyTask, newTask, updateTask } from "../controllers/task.js";
+import { isAuthenticated } from "../middlewares/auth.js";
+
+const router = express();
+
+router.post("/new",isAuthenticated,newTask);     
+
+router.get("/all",isAuthenticated,getMyTask);  
+
+router.route("/:id").put(isAuthenticated,updateTask).delete(isAuthenticated,deleteTask);
+
+export default router;
